fb7233dc94296072b39336eb974bdf71ede808e5
forest93 <forest93@163.com>
2017-10-29 21:47:44 +0800

    Add AbuseFilterList class.

---
+++ UnitTestProject1/Tests/AbuseFilterTests.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WikiClientLibrary.AbuseFilters;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace UnitTestProject1.Tests
+{
+    public class AbuseFilterTests : WikiSiteTestsBase
+    {
+
+        /// <inheritdoc />
+        public AbuseFilterTests(ITestOutputHelper output) : base(output)
+        {
+        }
+
+        [Fact]
+        public async Task AbuseFilterListTest()
+        {
+            var site = await WpTest2SiteAsync;
+            var aflist = new AbuseFilterList(site) {PaginationSize = 30};
+            var items = await aflist.EnumItemsAsync().ToList();
+            ShallowTrace(items);
+            Assert.Contains(items, f => f.LastEditor == "Luke081515");
+        }
+
+    }
+}
---
+++ WikiClientLibrary/AbuseFilters/AbuseFilter.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Newtonsoft.Json;
+
+namespace WikiClientLibrary.AbuseFilters
+{
+    [JsonObject(MemberSerialization.OptIn)]
+    public sealed class AbuseFilter
+    {
+        private ICollection<string> _Actions;
+
+        [JsonProperty]
+        public int Id { get; private set; }
+
+        [JsonProperty]
+        public string Description { get; set; }
+
+        [JsonProperty]
+        public string Pattern { get; set; }
+
+        public ICollection<string> Actions
+        {
+            get
+            {
+                if (_Actions == null) _Actions = new List<string>();
+                return _Actions;
+            }
+            set { _Actions = value; }
+        }
+
+        [JsonProperty("actions")]
+        private string RawActions
+        {
+            set { _Actions = string.IsNullOrEmpty(value) ? null : new List<string>(value.Split(',')); }
+        }
+
+        [JsonProperty]
+        public int Hits { get; private set; }
+
+        [JsonProperty]
+        public string Comments { get; set; }
+
+        [JsonProperty]
+        public string LastEditor { get; private set; }
+
+        [JsonProperty]
+        public DateTime LastEditTime { get; private set; }
+
+        [JsonProperty("deleted")]
+        public bool IsDeleted { get; set; }
+
+        [JsonProperty("private")]
+        public bool IsPrivate { get; set; }
+
+        [JsonProperty("enabled")]
+        public bool IsEnabled { get; set; }
+    }
+}
