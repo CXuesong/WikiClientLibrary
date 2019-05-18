625ea80b6c265e765fa1c8803aceb0737269f9cb
forest93 <forest93@163.com>
2019-05-18 15:46:54 +0800

    Add MediaWikiVersion struct.

---
+++ UnitTestProject1/Tests/MediaWikiVersionTests.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Text;
+using WikiClientLibrary;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace UnitTestProject1.Tests
+{
+    public class MediaWikiVersionTests : UnitTestsBase
+    {
+
+        /// <inheritdoc />
+        public MediaWikiVersionTests(ITestOutputHelper output) : base(output)
+        {
+        }
+
+        [Fact]
+        public void PerformanceTest()
+        {
+            const int INSTANCE_COUNT = 100000;
+            var rnd = new Random();
+            var inputs = new (int, int, int, int)[INSTANCE_COUNT];
+            var sysVersions = new Version[INSTANCE_COUNT];
+            var mwVersions = new MediaWikiVersion[INSTANCE_COUNT];
+            for (int i = 0; i < INSTANCE_COUNT; i++)
+                inputs[i] = (rnd.Next(32768), rnd.Next(32768), rnd.Next(32768), rnd.Next(0x0FFF));
+            GC.Collect();
+            var sw = Stopwatch.StartNew();
+            for (int i = 0; i < INSTANCE_COUNT; i++)
+            {
+                var (a, b, c, d) = inputs[i];
+                sysVersions[i] = new Version(a, b, c, d);
+            }
+            Output.WriteLine("System.Version used {0}.", sw.Elapsed);
+            sysVersions = null;
+            GC.Collect();
+            sw.Restart();
+            for (int i = 0; i < INSTANCE_COUNT; i++)
+            {
+                var (a, b, c, d) = inputs[i];
+                mwVersions[i] = new MediaWikiVersion(a, b, c);
+            }
+            Output.WriteLine("MediaWikiVersion used {0}.", sw.Elapsed);
+        }
+
+        [Fact]
+        public void ConstructionTest()
+        {
+            var v = new MediaWikiVersion(1, 29, 0, MediaWikiDevChannel.Wmf, 12);
+            Assert.Equal(1, v.Major);
+            Assert.Equal(29, v.Minor);
+            Assert.Equal(0, v.Revision);
+            Assert.Equal(MediaWikiDevChannel.Wmf, v.DevChannel);
+            Assert.Equal(12, v.DevVersion);
+            v = new MediaWikiVersion(32767, 32767, 32767, MediaWikiDevChannel.RC, 0x0FFF);
+            Assert.Throws<ArgumentOutOfRangeException>(() => new MediaWikiVersion(32768, 0, 0));
+            Assert.Throws<ArgumentOutOfRangeException>(() => new MediaWikiVersion(0, 32768, 0));
+            Assert.Throws<ArgumentOutOfRangeException>(() => new MediaWikiVersion(0, 0, 32768));
+            Assert.Throws<ArgumentOutOfRangeException>(() => new MediaWikiVersion(0, 0, 0, MediaWikiDevChannel.RC, 0x1000));
+            Assert.Throws<ArgumentException>(() => new MediaWikiVersion(0, 0, 0, MediaWikiDevChannel.None, 1));
+        }
+
+        [Fact]
+        public void ParseTest()
+        {
+            Assert.Equal(new MediaWikiVersion(1, 29, 0, MediaWikiDevChannel.Wmf, 12), MediaWikiVersion.Parse("1.29.0-wmf.12"));
+            Assert.Equal(new MediaWikiVersion(1, 0), MediaWikiVersion.Parse("1"));
+            Assert.Equal(new MediaWikiVersion(1, 28), MediaWikiVersion.Parse("1. 28"));
+            Assert.Equal(new MediaWikiVersion(1, 28, 0), MediaWikiVersion.Parse("1.28.0"));
+            Assert.Equal(new MediaWikiVersion(1, 28, 7), MediaWikiVersion.Parse("1.28.7"));
+            Assert.Equal(new MediaWikiVersion(1, 28, 0, MediaWikiDevChannel.Beta), MediaWikiVersion.Parse("1.28.0-beTa"));
+            Assert.Equal(new MediaWikiVersion(1, 28, 0, MediaWikiDevChannel.Wmf, 17), MediaWikiVersion.Parse("1.28.0-wmf.17"));
+            Assert.Equal(new MediaWikiVersion(1, 28, 0, MediaWikiDevChannel.Wmf, 17), MediaWikiVersion.Parse("1.28.0-wmf-17"));
+            Assert.Equal(new MediaWikiVersion(1, 28, 0, MediaWikiDevChannel.Wmf, 17), MediaWikiVersion.Parse("1.28.0-wmf17"));
+            Assert.Equal(new MediaWikiVersion(1, 28, 7), MediaWikiVersion.Parse("  1.28. 7  "));
+            // Unrecognizable DevVersion prefix: <space>wmf<space>
+            Assert.Throws<ArgumentException>(() => MediaWikiVersion.Parse("  1.28.7 - wmf .12"));
+        }
+
+        [Fact]
+        public void ComparisonTest()
+        {
+            Assert.True(MediaWikiVersion.Parse("1.0") > MediaWikiVersion.Zero);
+            Assert.True(MediaWikiVersion.Parse("1.29.1") > MediaWikiVersion.Parse("1.29"));
+            Assert.True(MediaWikiVersion.Parse("1.29.2") > MediaWikiVersion.Parse("1.29.1"));
+            Assert.True(MediaWikiVersion.Parse("1.28") > MediaWikiVersion.Parse("1.28-wmf"));
+            Assert.True(MediaWikiVersion.Parse("1.27") < MediaWikiVersion.Parse("1.28-wmf"));
+            Assert.True(MediaWikiVersion.Parse("1.28-wmf.1") > MediaWikiVersion.Parse("1.28-wmf"));
+            Assert.True(MediaWikiVersion.Parse("1.28-wmf.23") > MediaWikiVersion.Parse("1.28-wmf.22"));
+            Assert.True(MediaWikiVersion.Parse("1.28-alpha.1") > MediaWikiVersion.Parse("1.28-wmf.3"));
+            Assert.True(MediaWikiVersion.Parse("1.28-beta.1") > MediaWikiVersion.Parse("1.28-alpha.3"));
+            Assert.True(MediaWikiVersion.Parse("1.28-rc.1") > MediaWikiVersion.Parse("1.28-beta.3"));
+        }
+
+    }
+}
---
+++ WikiClientLibrary/MediaWikiVersion.cs
@@ -0,0 +1,283 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Text;
+
+namespace WikiClientLibrary
+{
+
+    public enum MediaWikiDevChannel
+    {
+        None = 0,
+        Wmf = 1,
+        Alpha = 2,
+        Beta = 3,
+        RC = 4
+    }
+
+    /// <summary>
+    /// Represents a MediaWiki core version number.
+    /// </summary>
+    /// <remarks>
+    /// <para>MediaWiki version number has the following format</para>
+    /// <para><var>major</var>.<var>minor</var>.<var>revision</var>[-<var>devChannel</var>.<var>devVersion</var>]</para>
+    /// </remarks>
+    public struct MediaWikiVersion : IEquatable<MediaWikiVersion>, IComparable<MediaWikiVersion>, IComparable
+    {
+
+        private static readonly char[] dashCharArray = { '-' };
+        private static readonly char[] dotCharArray = { '.' };
+
+        private readonly short _Major, _Minor, _Revision;
+        // bitmap for -wmf.3
+        // DevChannel | DevVersion
+        //       0001 | 0000 0000 0011
+        private readonly short fullDevVersion;
+
+        public static MediaWikiVersion Zero { get; } = default;
+
+        public static MediaWikiVersion Parse(string version)
+        {
+            if (string.IsNullOrEmpty(version))
+                throw new ArgumentException("Value cannot be null or empty.", nameof(version));
+            // 1.0.0-rc-1
+            var components1 = version.Split(dashCharArray, 2);
+            var components2 = components1[0].Split(dotCharArray, 4);
+            if (components2.Length > 3)
+                throw new ArgumentException("Version has too many components.", nameof(version));
+            var major = short.Parse(components2[0]);
+            short minor = 0, revision = 0;
+            var devChannel = MediaWikiDevChannel.None;
+            var devVersion = 0;
+            if (components2.Length > 1)
+                minor = short.Parse(components2[1]);
+            if (components2.Length > 2)
+                revision = short.Parse(components2[2]);
+
+            int ParseDevVersion(string expr, int startsFrom)
+            {
+                if (expr.Length == startsFrom)
+                    return 0;
+                if (expr[startsFrom] == '.' || expr[startsFrom] == '-')
+                    startsFrom++;
+                // If . or - detected, we expect a number after it.
+                if (expr.Length <= startsFrom)
+                    throw new ArgumentException("Incomplete DevVersion expression.", nameof(version));
+                return int.Parse(expr.Substring(startsFrom));
+            }
+            if (components1.Length > 1)
+            {
+                var devFullVersion = components1[1];
+                if (devFullVersion.StartsWith("wmf", StringComparison.OrdinalIgnoreCase))
+                {
+                    devChannel = MediaWikiDevChannel.Wmf;
+                    devVersion = ParseDevVersion(devFullVersion, 3);
+                }
+                else if (devFullVersion.StartsWith("rc", StringComparison.OrdinalIgnoreCase))
+                {
+                    devChannel = MediaWikiDevChannel.RC;
+                    devVersion = ParseDevVersion(devFullVersion, 2);
+                }
+                else if (devFullVersion.StartsWith("alpha", StringComparison.OrdinalIgnoreCase))
+                {
+                    devChannel = MediaWikiDevChannel.Alpha;
+                    devVersion = ParseDevVersion(devFullVersion, 5);
+                }
+                else if (devFullVersion.StartsWith("beta", StringComparison.OrdinalIgnoreCase))
+                {
+                    devChannel = MediaWikiDevChannel.Beta;
+                    devVersion = ParseDevVersion(devFullVersion, 4);
+                }
+                else
+                {
+                    throw new ArgumentException($"Unrecognizable DevVersion prefix: {devFullVersion}.");
+                }
+            }
+            return new MediaWikiVersion(major, minor, revision, devChannel, devVersion);
+        }
+
+        private MediaWikiVersion(short major, short minor, short revision, MediaWikiDevChannel devChannel, int devVersion)
+        {
+            if (major < 0)
+                throw new ArgumentOutOfRangeException(nameof(major));
+            if (minor < 0)
+                throw new ArgumentOutOfRangeException(nameof(minor));
+            if (revision < 0)
+                throw new ArgumentOutOfRangeException(nameof(revision));
+            if (devVersion < 0 || devVersion > 0x0FFF)
+                throw new ArgumentOutOfRangeException(nameof(devVersion));
+            _Major = major;
+            _Minor = minor;
+            _Revision = revision;
+            fullDevVersion = (short)(((int)devChannel << 12) | devVersion);
+        }
+        public MediaWikiVersion(int major, int minor) 
+            : this(major, minor, 0, MediaWikiDevChannel.None, 0)
+        {
+        }
+
+        public MediaWikiVersion(int major, int minor, int revision) 
+            : this(major, minor, revision, MediaWikiDevChannel.None, 0)
+        {
+        }
+
+        public MediaWikiVersion(int major, int minor, int revision, MediaWikiDevChannel devChannel) 
+            : this(major, minor, revision, devChannel, 0)
+        {
+        }
+
+        public MediaWikiVersion(int major, int minor, int revision, MediaWikiDevChannel devChannel, int devVersion)
+        {
+            if (major < 0 || major > short.MaxValue)
+                throw new ArgumentOutOfRangeException(nameof(major));
+            if (minor < 0 || minor > short.MaxValue)
+                throw new ArgumentOutOfRangeException(nameof(minor));
+            if (revision < 0 || revision > short.MaxValue)
+                throw new ArgumentOutOfRangeException(nameof(revision));
+            if (devChannel < MediaWikiDevChannel.None || devChannel > MediaWikiDevChannel.RC)
+                throw new ArgumentOutOfRangeException(nameof(devChannel));
+            if (devVersion < 0 || devVersion > 0x0FFF)
+                throw new ArgumentOutOfRangeException(nameof(devVersion));
+            _Major = (short)major;
+            _Minor = (short)minor;
+            _Revision = (short)revision;
+            if (devChannel == MediaWikiDevChannel.None)
+            {
+                if (devVersion != 0)
+                    throw new ArgumentException("When devChannel is None, devVersion must be 0.", nameof(devVersion));
+                fullDevVersion = 0;
+            }
+            else
+            {
+                fullDevVersion = (short)(((int)devChannel << 12) | devVersion);
+            }
+        }
+
+        public int Major => _Major;
+
+        public int Minor => _Minor;
+
+        public int Revision => _Revision;
+
+        public MediaWikiDevChannel DevChannel => (MediaWikiDevChannel)(fullDevVersion >> 12);
+
+        public int DevVersion => fullDevVersion & 0x0FFF;
+
+        /// <inheritdoc />
+        public override int GetHashCode()
+        {
+            return unchecked(_Major * 113 + _Minor * 57 + _Revision * 23 + fullDevVersion);
+        }
+
+
+        /// <inheritdoc />
+        public int CompareTo(MediaWikiVersion other)
+        {
+            if (_Major > other._Major) return 1;
+            if (_Major < other._Major) return -1;
+            if (_Minor > other._Minor) return 1;
+            if (_Minor < other._Minor) return -1;
+            if (_Revision > other._Revision) return 1;
+            if (_Revision < other._Revision) return -1;
+            // fullDevVersion != 0: wmf/alpha/beta/rc
+            if (fullDevVersion == 0)
+                return other.fullDevVersion == 0 ? 0 : 1;
+            if (other.fullDevVersion == 0)
+                // We have fullDevVersion != 0 here.
+                return -1;
+            if (fullDevVersion > other.fullDevVersion) return 1;
+            if (fullDevVersion < other.fullDevVersion) return -1;
+            return 0;
+        }
+
+        /// <inheritdoc />
+        public override string ToString()
+        {
+            var sb = new StringBuilder(20);
+            sb.Append(Major);
+            sb.Append('.');
+            sb.Append(Minor);
+            sb.Append('.');
+            sb.Append(Revision);
+            var channel = DevChannel;
+            if (channel != MediaWikiDevChannel.None)
+            {
+                switch (DevChannel)
+                {
+                    case MediaWikiDevChannel.Wmf:
+                        sb.Append("-wmf");
+                        break;
+                    case MediaWikiDevChannel.Alpha:
+                        sb.Append("-alpha");
+                        break;
+                    case MediaWikiDevChannel.Beta:
+                        sb.Append("-beta");
+                        break;
+                    case MediaWikiDevChannel.RC:
+                        sb.Append("-rc");
+                        break;
+                    default:
+                        Debug.Assert(false, "Invalid DevChannel value.");
+                        sb.Append("-invalid");
+                        break;
+                }
+                var devVersion = DevVersion;
+                if (devVersion > 0)
+                {
+                    sb.Append('.');
+                    sb.Append(devVersion);
+                }
+            }
+            return sb.ToString();
+        }
+
+        /// <inheritdoc />
+        public bool Equals(MediaWikiVersion other)
+        {
+            return _Major == other._Major && _Minor == other._Minor && _Revision == other._Revision && fullDevVersion == other.fullDevVersion;
+        }
+
+        /// <inheritdoc />
+        public override bool Equals(object obj)
+        {
+            return obj is MediaWikiVersion other && Equals(other);
+        }
+
+        public static bool operator ==(MediaWikiVersion left, MediaWikiVersion right)
+        {
+            return left.Equals(right);
+        }
+
+        public static bool operator !=(MediaWikiVersion left, MediaWikiVersion right)
+        {
+            return !left.Equals(right);
+        }
+
+        /// <inheritdoc />
+        public int CompareTo(object obj)
+        {
+            if (ReferenceEquals(null, obj)) return 1;
+            return obj is MediaWikiVersion other ? CompareTo(other) : throw new ArgumentException($"Object must be of type {nameof(MediaWikiVersion)}");
+        }
+
+        public static bool operator <(MediaWikiVersion left, MediaWikiVersion right)
+        {
+            return left.CompareTo(right) < 0;
+        }
+
+        public static bool operator >(MediaWikiVersion left, MediaWikiVersion right)
+        {
+            return left.CompareTo(right) > 0;
+        }
+
+        public static bool operator <=(MediaWikiVersion left, MediaWikiVersion right)
+        {
+            return left.CompareTo(right) <= 0;
+        }
+
+        public static bool operator >=(MediaWikiVersion left, MediaWikiVersion right)
+        {
+            return left.CompareTo(right) >= 0;
+        }
+    }
+}
