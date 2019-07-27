527ae32e9e69dfdf2136e5a57f4ffafd51bb0bee
forest93 <forest93@163.com>
2019-07-27 17:35:31 +0800

    Use pwsh to install dotnet core 3.

--- WikiClientLibrary.Commons/CI/Install.ps1
+++ WikiClientLibrary.Commons/CI/Install.ps1
@@ -1,6 +1,26 @@
 trap {
     Write-Error $_
+    Write-Host $_.ScriptStackTrace
     Exit 1
 }
+function CheckLastExitCode() {
+    if ($LASTEXITCODE) {
+        Write-Host (Get-PSCallStack)
+        Exit $LASTEXITCODE
+    }
+}
 # Assumes $PWD is the repo root
-Exit $LASTEXITCODE
+if ($IsLinux) {
+    # Install .NET Core 3.0 preview
+    Invoke-WebRequest https://dotnetcli.blob.core.windows.net/dotnet/Sdk/release/3.0.1xx/dotnet-sdk-latest-linux-x64.tar.gz -OutFile dotnet.tar.gz
+    mkdir /usr/share/dotnet
+    tar -zxf dotnet.tar.gz -C /usr/share/dotnet
+    CheckLastExitCode
+    if (-not (Get-Command dotnet))
+    {
+        New-Item /usr/share/dotnet/dotnet -ItemType SymbolicLink -Value /usr/bin/dotnet
+    }
+    Write-Host "Installed .NET Core SDKs:"
+    dotnet --list-sdks
+    CheckLastExitCode
+}
