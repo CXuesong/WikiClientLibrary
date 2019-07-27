f944ab4f02605caf4a014a8c943a7f5449a6dd3d
forest93 <forest93@163.com>
2019-07-27 14:52:35 +0800

    Use a stable dotnet build.

--- WikiClientLibrary.Commons/CI/Build.ps1
+++ WikiClientLibrary.Commons/CI/Build.ps1
@@ -2,9 +2,12 @@ trap {
     Write-Error $_
     Exit 1
 }
-if ($env:BUILD_SECRET_KEY) {
-    &"$PSScriptRoot/BuildSecret.ps1" -Restore -Key $env:BUILD_SECRET_KEY
-}
 # Assumes $PWD is the repo root
+if ($env:BUILD_SECRET_KEY) {
+    &"$PSScriptRoot/BuildSecret.ps1" -Restore -SourceRootPath . -SecretPath $PSScriptRoot/Secret.bin -Key $env:BUILD_SECRET_KEY
+} else {
+    Write-Warning "BUILD_SECRET_KEY is not available. Will build without secret."
+}
+
 dotnet build WikiClientLibrary.sln -c CIRelease
 Exit $LASTEXITCODE
