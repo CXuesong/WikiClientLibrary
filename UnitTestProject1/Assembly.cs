53bb1c5699190dfa0954621dcfe0bff1036552ee
forest93 <forest93@163.com>
2019-07-29 00:38:47 +0800

    Formatting.

--- UnitTestProject1/Assembly.cs
+++ UnitTestProject1/Assembly.cs
@@ -4,5 +4,5 @@
 // https://github.com/CXuesong/WikiClientLibrary/issues/11
 // We are using Bot Password on CI, which may naturally evade the issue.
 #if !ENV_CI_BUILD
-[assembly:CollectionBehavior(DisableTestParallelization = true)]
+[assembly: CollectionBehavior(DisableTestParallelization = true)]
 #endif
