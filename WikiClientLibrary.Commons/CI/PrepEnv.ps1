d5a0cbd9ad9e47c8a1e682f7da7f87c98f78b4b3
forest93 <forest93@163.com>
2019-11-16 01:12:12 +0800

    Install SHFB for Release env.

--- WikiClientLibrary.Commons/CI/PrepEnv.ps1
+++ WikiClientLibrary.Commons/CI/PrepEnv.ps1
@@ -1,12 +1,16 @@
+param (
+    [string]
+    $Configuration = "CIRelease"
+)
 trap {
     Write-Error $_
     Write-Host $_.ScriptStackTrace
     Exit 1
 }
-function CheckLastExitCode() {
-    if ($LASTEXITCODE) {
+function CheckLastExitCode($ExitCode = $LASTEXITCODE) {
+    if ($ExitCode) {
         Write-Host (Get-PSCallStack)
-        Exit $LASTEXITCODE
+        Exit $ExitCode
     }
 }
 
@@ -17,13 +21,29 @@ if ($IsLinux) {
     Write-Host "Installed .NET Core SDKs:"
     dotnet --list-sdks
     CheckLastExitCode
-} elseif ($IsWindows) {
+}
+elseif ($IsWindows) {
+    # dotnet
     Invoke-WebRequest 'https://dot.net/v1/dotnet-install.ps1' -OutFile 'DotNet-Install.ps1'
     ./DotNet-Install.ps1 -Version 2.1.202
     ./DotNet-Install.ps1 -Version 3.0.100
     Write-Host "Installed .NET Core SDKs:"
     dotnet --list-sdks
     CheckLastExitCode
-} else {
+
+    # SHFB
+    if ($Configuration -eq "Release") {
+        Invoke-WebRequest "https://github.com/EWSoftware/SHFB/releases/download/v2019.9.15.0/SHFBInstaller_v2019.9.15.0.zip" -OutFile SHFBInstaller.zip
+        New-Item -ItemType Directory SHFBInstaller
+        Expand-Archive SHFBInstaller.zip SHFBInstaller
+        $proc = Start-Process -PassThru -Wait -FilePath ./SHFBInstaller/InstallResources/SandcastleHelpFileBuilder.msi -ArgumentList /quiet, /lwe, SHFBInstall.log
+        Get-Content SHFBInstall.log
+        CheckLastExitCode ($proc.ExitCode)
+    }
+    else {
+        Write-Host "Do not install SHFB on non-Release environment."
+    }
+}
+else {
     Write-Error "Invalid Environment."
 }
