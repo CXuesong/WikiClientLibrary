5250b212e6a722eb8f929dae2b6e726a53ca0bd1
forest93 <forest93@163.com>
2019-11-16 15:12:25 +0800

    Explicitly install SHFB.

--- WikiClientLibrary.Commons/CI/PrepEnv.ps1
+++ WikiClientLibrary.Commons/CI/PrepEnv.ps1
@@ -1,7 +1,10 @@
 param (
-    [string]
-    $Configuration = "CIRelease"
+    [switch]
+    $SHFB = $false
 )
+
+$ErrorActionPreference = "Stop"
+
 trap {
     Write-Error $_
     Write-Host $_.ScriptStackTrace
@@ -16,6 +19,9 @@ function CheckLastExitCode($ExitCode = $LASTEXITCODE) {
 
 # Assumes $PWD is the repo root
 if ($IsLinux) {
+    if ($SHFB) {
+        Write-Error "SHFB is not supported on Linux."
+    }
     sudo apt install dotnet-sdk-2.1.202 dotnet-sdk-3.0
     CheckLastExitCode
     Write-Host "Installed .NET Core SDKs:"
@@ -32,16 +38,15 @@ elseif ($IsWindows) {
     CheckLastExitCode
 
     # SHFB
-    if ($Configuration -eq "Release") {
+    if ($SHFB) {
+        Write-Host "Downloading SHFB."
         Invoke-WebRequest "https://github.com/EWSoftware/SHFB/releases/download/v2019.9.15.0/SHFBInstaller_v2019.9.15.0.zip" -OutFile SHFBInstaller.zip
         New-Item -ItemType Directory SHFBInstaller | Out-Null
         Expand-Archive SHFBInstaller.zip SHFBInstaller
+        Write-Host "Downloading SHFB."
         $proc = Start-Process -PassThru -Wait -FilePath ./SHFBInstaller/InstallResources/SandcastleHelpFileBuilder.msi -ArgumentList /quiet, /lwe, SHFBInstall.log
         Get-Content SHFBInstall.log
         CheckLastExitCode ($proc.ExitCode)
-    }
-    else {
-        Write-Host "Do not install SHFB on non-Release environment."
     }
 }
 else {
