176822a31bf3c15a5732d66a49ceabc2af582794
forest93 <forest93@163.com>
2021-01-24 23:30:12 +0800

    LinqToCargo sample: Show more columns.

--- Samples/LinqToCargo/Program.cs
+++ Samples/LinqToCargo/Program.cs
@@ -9,9 +9,7 @@ using WikiClientLibrary.Cargo.Schema;
 using WikiClientLibrary.Client;
 using WikiClientLibrary.Sites;
 
-using var client = new WikiClient { ClientUserAgent = "WikiClientLibrary.Samples.CargoLinq/1.0" };
-var site = new WikiSite(client, "https://lol.gamepedia.com/api.php");
-site.Logger = LoggerFactory.Create(c => c
+var loggerFactory = LoggerFactory.Create(c => c
     // Use LogLevel.Debug to see the exact query parameters.
     .SetMinimumLevel(LogLevel.Debug)
     .AddSimpleConsole(c1 =>
@@ -19,14 +17,25 @@ site.Logger = LoggerFactory.Create(c => c
         c1.ColorBehavior = LoggerColorBehavior.Enabled;
         c1.IncludeScopes = true;
     })
-).CreateLogger<WikiSite>();
+);
+using var client = new WikiClient
+{
+    ClientUserAgent = "WikiClientLibrary.Samples.CargoLinq/1.0",
+    Logger = loggerFactory.CreateLogger<WikiClient>()
+};
+var site = new WikiSite(client, "https://lol.gamepedia.com/api.php");
+site.Logger = loggerFactory.CreateLogger<WikiSite>();
 await site.Initialization;
 
-// Query starts here.
 var context = new LolCargoQueryContext(site);
+// Query starts here.
 var query = context.RosterChanges
     .Where(x => x.DateSort > new DateTime(2020, 12, 11))
     .OrderBy(x => x.DateSort)
+    .Take(100)
+    // Cargo query expression ends here.
+    .AsAsyncEnumerable()
+    // Do some local conversion for better display on console.
     .Select(x => new
     {
         x.Page,
@@ -36,10 +45,9 @@ var query = context.RosterChanges
         Roles = string.Join(';', x.Roles),
         Tags = string.Join(';', x.Tags),
         Tournaments = string.Join(';', x.Tournaments)
-    })
-    .Take(100);
+    });
 var counter = 0;
-await foreach (var item in query.AsAsyncEnumerable())
+await foreach (var item in query)
 {
     counter++;
     Console.WriteLine("{0,4}: {1}", counter, item);
